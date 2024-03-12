94e3a4e0eb40914e966be12e8c8862660a3a4e8a
winner umeh <winniffy@gmail.com>
2024-03-12 13:28:39 +0100

    add favicon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,24 @@
 import "./Footer.css";
+import XIcon from "@mui/icons-material/X";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 const Footer = ({ logo }) => {
   return <Footer classname="footer">
 <div className="fooer_top">
     <img src={logo} alt="armeni logo" />
     <ul className="footer_nav">
-        <a href="">
-        <li>Buy $Armeni</li>
+        <a href="https://jup.ag/swap/SOL-ARMENI_5AJjM9VRFRDawVcCi7bsT56bMUij3iXcuovH7mxN1Spb" target="_blank"
+            rel="noopener noreferrer">
+        <li>Buy $ARMENI</li>
         </a>
-        <a href="">
+        <a href="https://birdeye.so/token/5AJjM9VRFRDawVcCi7bsT56bMUij3iXcuovH7mxN1Spb?chain=solana" target="_blank"
+            rel="noopener noreferrer">
         <li>View Chart</li>
         </a>
     </ul>
     <div className="footer_social">
-
+        <XIcon/>
+        <TelegramIcon/>
     </div>
 
     <p className="footer_copyright">
