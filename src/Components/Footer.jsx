9b19401b78a545c996a82c54d5d659c36711ad78
winner umeh <winniffy@gmail.com>
2024-03-12 12:13:00 +0100

    start footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,26 @@
-import ".Footer.css";
+import "./Footer.css";
 
 const Footer = ({ logo }) => {
-  return <Footer classname="footer">Footer</Footer>;
+  return <Footer classname="footer">
+<div className="fooer_top">
+    <img src={logo} alt="armeni logo" />
+    <ul className="footer_nav">
+        <a href="">
+        <li>Buy $Armeni</li>
+        </a>
+        <a href="">
+        <li>View Chart</li>
+        </a>
+    </ul>
+    <div className="footer_social">
+
+    </div>
+
+    <p className="footer_copyright">
+        Copyright &copy; 2024 $Armeni - All rights reserved
+    </p>
+</div>
+  </Footer>;
 };
 
 export default Footer;
